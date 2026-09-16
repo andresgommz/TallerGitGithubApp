853793efba7580e244e47970df3c8acab011b8cc
Andrés Gómez <agomez228@unab.edu.co>
2026-09-16 09:25:15 -0500

    feat: agrega imagen personal

--- app/src/main/java/me/andresgomez/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/andresgomez/tallergitgithubapp/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.andresgomez.tallergitgithubapp.ui.theme.TallerGitGithubAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,8 +56,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TallerGitGithubAppTheme {
-        Greeting("Android")
+fun TarjetaPresentacion() {
+    Card {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            val painter = painterResource(id = R.drawable.andres)
+            val contentDescription = "Imagen Andrés"
+
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                modifier = Modifier
+                    .padding(
+                        top = 24.dp
+                    )
+                    .clip(CircleShape)
+                    .size(120.dp)
+                    .border(
+                        width = 3.dp,
+                        color = Color(0xFF3B89D8),
+                        shape = CircleShape
+                    )
+            )
+        }
     }
 }
